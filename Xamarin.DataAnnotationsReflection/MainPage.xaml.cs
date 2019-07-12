d5e67c50c6e809de4af1542245b53bf1f1bf2596
Alexey Strakh <alexey.strakh@microsoft.com>
2019-07-12 12:11:12 -0700

    data annotations and reflection

---
+++ Xamarin.DataAnnotationsReflection/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+using Xamarin.Forms;
+
+namespace Xamarin.DataAnnotationsReflection
+{
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Submit_Click(object sender, EventArgs e)
+        {
+            var model = this.BindingContext;
+            var props = model.GetType().GetProperties(BindingFlags.Public | BindingFlags.Instance);
+            var validationProps = props.Select(p => new { Property = p, ValidationAttribute = p.GetCustomAttribute<ValidationAttribute>() })
+                .Where(p => p.ValidationAttribute != null)
+                .ToList();
+
+            System.Diagnostics.Debug.WriteLine($"Found {validationProps.Count} validation properties: {string.Join(",", validationProps.Select(vp => vp.Property.Name))}");
+        }
+    }
+}
---
+++ Xamarin.DataAnnotationsReflection/MyViewModel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Xamarin.DataAnnotationsReflection
+{
+    public class MyViewModel : INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        private string _myTextEntry;
+        [MaxLength(10)]
+        public string MyTextEntry
+        {
+            get { return _myTextEntry; }
+            set
+            {
+                if (_myTextEntry == value)
+                    return;
+
+                _myTextEntry = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(MyTextEntry)));
+                System.Diagnostics.Debug.WriteLine($"My Entry Text Changed to: {value}");
+                ValidateModel();
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void ValidateModel()
+        {
+            _validationErrors.Clear();
+            var context = new ValidationContext(this);
+            var valid = Validator.TryValidateObject(this, context, _validationErrors, true);
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(HasErrors)));
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(nameof(MyTextEntry)));
+            System.Diagnostics.Debug.WriteLine($"Validation has been completed: {valid}, errors: {_validationErrors.Count}");
+        }
+
+        private readonly List<ValidationResult> _validationErrors = new List<ValidationResult>();
+
+        public bool HasErrors => _validationErrors.Count > 0;
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        public IEnumerable GetErrors(string propertyName)
+        {
+            var errors = new List<string>();
+            if (HasErrors)
+            {
+                errors = _validationErrors
+                    .Where(e => e.MemberNames.Contains(propertyName))
+                    .Select(e => e.ErrorMessage)
+                    .ToList();
+            }
+
+            return errors;
+        }
+    }
+}
